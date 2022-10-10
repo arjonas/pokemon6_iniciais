99e45326c9fa1fcb6a6586a6d9c85eef30124de9
arjonas <sidnei.arjonas@gmail.com>
2022-10-09 21:49:37 -0300

    'Adicionado arquivos iniciais criados para ideia de expanção do site .. '

--- app.py
+++ app.py
@@ -1,7 +1,53 @@
-from flask import  Flask, render_template
+from flask import  Flask, render_template,request,redirect,url_for
 import requests
 from random import randint
-from jinja2 import escape
+
+from flask_login import LoginManager
+
+
+endpoint = 'https://pokeapi.co/api/v2/pokemon/'
+
+
+Kanto =  [('bulbasaur' ,'ivysaur','venusaur'), ('charmander','charmeleon','charizard') ,
+          ('squirtle','wartortle','blastoise')]
+
+
+Johto = [('chikorita','bayleef','meganium'), ('cyndaquil','quilava','typhlosion')
+                                              ,('totodile','croconaw','feraligatr')]
+
+Hoenn = [('treecko','grovyle','sceptile'), ('torchic','combusken','blaziken'),
+         ('mudkip','marshtomp','swampert')]
+
+
+
+
+Sinnoh = [('turtwig','grotle','torterra') ,('chimchar','monferno','infernape'),
+          ('piplup','prinplup','empoleon')]
+
+Unova = [('snivy','servine','serperior'), ('tepig','pignite','emboar'),
+         ('oshawott','dewott','samurott')]
+#
+Kalos = [('chespin','quilladin','chesnaught'), ('fennekin','braixen','delphox') ,
+         ('froakie','frogadier','greninja')]
+
+Alola =  [('rowlet','dartrix','decidueye') ,('litten','torracat','incineroar') ,
+          ('popplio','brionne','primarina')]
+
+
+Galar = [('grookey' ,'scorbunny','sobble')]
+
+regioes = [Kanto,Johto,Hoenn,Sinnoh,Unova,Kalos,Alola,Galar]
+
+
+
+
+
+
+
+
+
+
+
 
 
 endpoint = 'https://pokeapi.co/api/v2/pokemon/'
@@ -13,7 +59,7 @@ app = Flask('__name__')
 
 
 
-@app.route('/')
+@app.route('/data')
 def get_pokemons():
 
     lista = []
@@ -48,6 +94,72 @@ def get_pokemons():
 
     return render_template('pokemons.html',dados=data_pokemon)
 
+
+@app.route('/home')
+def logar():
+
+   return render_template('index.html')
+
+
+
+
+@app.route('/',methods=('GET','POST'))
+def choice():\
+
+    if request.method == 'POST':
+
+
+        #coletando dados de formulario e gerando imagens dos pokemons iniciais
+        data = request.form.get('city')
+
+
+        city = regioes[int(data)]
+
+        iniciais = [item[0] for item in city ]
+
+        init_img = []
+        for pokemon in iniciais :
+
+            resposta = requests.get(endpoint+pokemon)
+
+            info = resposta.json()
+
+            img = info['sprites']['front_default']
+
+            init_img.append(img)
+
+        return render_template('pokemonchoice.html' , imgs = init_img)
+
+
+
+
+
+    return render_template('Start.html')
+
+
+@app.route('/pk',methods=('GET','POST'))
+def inicio():
+
+    if request.method == "POST":
+
+        return  redirect(url_for('jornada'))
+
+
+
+    return render_template('pokemonchoice.html')
+
+
+
+
+@app.route('/jorney',methods=('GET','POST'))
+def jornada():
+
+    if request.method == 'POST':
+
+        return redirect(url_for('get_pokemons'))
+
+
+    return render_template('choice.html')
 
 
 
